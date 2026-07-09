2433e3786b7b64170c35837bc60a686cd630fd74
orbisai0security <mediratta01.pally@gmail.com>
2026-07-09 00:26:40 +0000

    fix: c.lang.security.insecure-use-string-copy-fn.insecure-use-string-copy-fn security vulnerability
    Automated security fix generated by OrbisAI Security

--- gputemps.c
+++ gputemps.c
@@ -314,7 +314,7 @@ static void replace_suffix(
     if (len - suffix_len + replacement_len >= s_size)
         return;
 
-    strcpy(s + len - suffix_len, replacement);
+    memcpy(s + len - suffix_len, replacement, replacement_len + 1);
 }
 
 static void make_short_name(const char *name, char *out, size_t out_size) {
